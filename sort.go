1e6864710696e5dd64dcae9e80e5c5bfb99cb9c2
Dimitri Sokolyuk <sokolyuk@gmail.com>
2021-12-17 01:00:49 +0100

    Improvement

--- sort.go
+++ sort.go
@@ -4,8 +4,8 @@ import "constraints"
 
 func Sort[T constraints.Ordered](v []T) {
 	n := len(v)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
+	for i := 1; i < n; i++ {
+		for j := 0; j < i; j++ {
 			if v[i] < v[j] {
 				v[i], v[j] = v[j], v[i]
 			}
