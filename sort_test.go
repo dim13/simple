b3e9d8b1d4bbe57ed20922d2bd9cfd02358ea326
Dimitri Sokolyuk <sokolyuk@gmail.com>
2022-02-09 14:45:11 +0100

    Add slices Sort

--- sort_test.go
+++ sort_test.go
@@ -3,6 +3,8 @@ package simple
 import (
 	"sort"
 	"testing"
+
+	"golang.org/x/exp/slices"
 )
 
 func TestSort(t *testing.T) {
@@ -22,6 +24,12 @@ func BenchmarkSort(b *testing.B) {
 		for n := 0; n < b.N; n++ {
 			a := []int{1, 4, 9, 13, 2, 5, 8, 7}
 			Sort(a)
+		}
+	})
+	b.Run("slice", func(b *testing.B) {
+		for n := 0; n < b.N; n++ {
+			a := []int{1, 4, 9, 13, 2, 5, 8, 7}
+			slices.Sort(a)
 		}
 	})
 }
